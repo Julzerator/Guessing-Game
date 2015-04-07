eea5a7524f3f1bfb351ad0b445335b70a6c56ab6
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 16:08:31 -0700

    pseudo code beginnings

---
+++ game.py
@@ -0,0 +1,13 @@
+# Put your code here
+# greet player
+
+# get player name
+
+# choose random number between 1 and 100
+
+# while True:
+#   get guess
+#   if guess is incorrect:
+#       give hint
+#   else:
+#       congratulate player
