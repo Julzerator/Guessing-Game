827d6e98235f88cd2f2c0c7224d38648359a1aed
Hackbright Student <info@hackbrightacademy.com>
2015-04-07 18:14:43 -0700

    extra credit: Ask if you want to play again!

--- game.py
+++ game.py
@@ -6,10 +6,11 @@ print "Hello Numbers Master!"
 print "What's your name?",
 name = raw_input()
 # choose random number between 1 and 100
-print "I've chosen a number between 1 and 100."
-num = int(random.randrange(100)) + 1
+while True:
+    print "I've chosen a number between 1 and 100."
+    num = int(random.randrange(100)) + 1
 
-print "%s, guess what number I've chosen!" % name, 
+    print "%s, guess what number I've chosen!" % name, 
 
 # while True:
 #   get guess
@@ -17,19 +18,28 @@ print "%s, guess what number I've chosen!" % name,
 #       give hint
 #   else:
 #       congratulate player
-while True:
-    try:
-        guess = int(raw_input())
-        if guess > 100:
-            print "Your guess must be between 1 and 100. Dolt. Please try again."
-        elif guess < num:
-            print "Your guess is too low. Try again.",
-        elif guess > num:
-            print "Your guess is too high. Try again.",
-        else:
-            break
-    except ValueError:
-        print "I do not compute anything but numbers. What is this garbage?"
-        print "Please try again."
 
-print "Congratulations! You are now the Numbers Master!!"
+    while True:
+        try:
+            guess = int(raw_input())
+            if guess > 100:
+                print "Your guess must be between 1 and 100. Dolt. Please try again."
+            elif guess < num:
+                print "Your guess is too low. Try again.",
+            elif guess > num:
+                print "Your guess is too high. Try again.",
+            else:
+                print "Congratulations! You are now the Numbers Master!!"
+                break
+        except ValueError:
+            print "I do not compute anything but numbers. What is this garbage?"
+            print "Please try again."
+
+    print "Would you like to play again? Yes or No:",
+    answer = raw_input()
+    if answer != "Yes" and answer != "No":
+        print "Please answer Yes or No:"
+    elif answer == "Yes":
+        print "Yay! Let's Play!"
+    elif answer == "No":
+        break
